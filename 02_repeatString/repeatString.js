db04f528ef461d059c33a1905cf966cd594f908d
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-24 18:08:13 +0200

    implement logic for repeating strings in repeatString() method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function(string, repeatCount) {
-    
+    let repeatedString = "";
+
+    for (let i = 0; i < repeatCount; i++)
+        repeatedString += string;
+
+    return repeatedString;
 };
 
 // Do not edit below this line

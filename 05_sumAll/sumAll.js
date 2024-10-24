c75cee233b6d3ccbef5cbe5472643a8b58fe1570
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-24 19:15:11 +0200

    fix sumAll() not throwing error if args are not Numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,5 @@
 const sumAll = function (num1, num2) {
+    if (!isNumber(num1) || !isNumber(num2)) return "ERROR";
     if (num1 < 0 || num2 < 0) return "ERROR";
 
     let sum = 0;
@@ -8,6 +9,10 @@ const sumAll = function (num1, num2) {
     }
 
     return sum;
+};
+
+const isNumber = function (num) {
+    return Number(num) === num;
 };
 
 // Do not edit below this line

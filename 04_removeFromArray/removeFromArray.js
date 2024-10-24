36dc9fd24bd5bb89ace0635b797f5a11c6ff3217
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-24 18:27:42 +0200

    implement logic for removing element from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementValue) {
+    const index = array.indexOf(elementValue);
+    array.splice(index, 1);
+
+    return array;
 };
 
 // Do not edit below this line

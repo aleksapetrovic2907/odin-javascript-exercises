476f47e88fdae9a0482c5f1ee29daba75222ac6b
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-24 19:31:51 +0200

    fix precision point issues

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function (temperature) {
-  return ((temperature - 32) * (5 / 9));
+  return Math.round(((temperature - 32) * (5 / 9)) * 10) / 10;
 };
 
 const convertToFahrenheit = function (temperature) {
-  return ((temperature * 9 / 5) + 32);
+  return Math.round(((temperature * 9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line

da276c9ff850667f963a84afb80353df20d14d21
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-26 01:21:49 +0200

    implement method for filtering only alphabet chars in a string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,20 @@
 const palindromes = function (string) {
+    string = filterString(string);
     for (let i = 0; i < string.length; i++) {
         if (string[i] !== string[string.length - i - 1]) return false;
     }
 
     return true;
 };
+
+// Removes spaces, symbols and sets everything to lowercase.
+function filterString(string) {
+    const isAlphabetRegex = /^[a-zA-Z]+$/;
+
+    string = string.split('');
+    string = string.filter((ch) => isAlphabetRegex.test(ch)).join('');
+    return string.toLowerCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;

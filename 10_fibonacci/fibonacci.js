d2a69d23876b9b05305b4d02b8cd73334e7f3295
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-26 01:32:13 +0200

    fix formatting issue

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -2,7 +2,7 @@ const fibonacci = function (index) {
     if (index < 0) return "OOPS";
     if (index == 0) return 0;
     if (index == 1 || index == 2) return 1;
-    return fibonacci (index - 1) + fibonacci (index - 2);
+    return fibonacci(index - 1) + fibonacci(index - 2);
 };
 
 // Do not edit below this line

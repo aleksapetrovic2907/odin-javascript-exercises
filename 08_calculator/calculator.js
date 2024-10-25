a80e0183cb8414a4bbb4ad3d29ab7ac892ea64a5
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2024-10-26 01:03:26 +0200

    implement logic for multiplying elements of an array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,7 +10,8 @@ const sum = function(array) {
 	return array.reduce((accumulated, current) => accumulated + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((accumulated, current) => accumulated * current);
 };
 
 const power = function() {
